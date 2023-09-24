53a1f4d0a1303b70692ea620dd1058767dc6faf2
Brooklyn <bpater1@jh.edu>
2023-09-24 17:04:36 -0400

    now time to work on q4 with correct input files

---
+++ HW2/hw2q6.py
@@ -0,0 +1,4 @@
+# taking late days so don't have time to complete this problem 
+# but plan to do so as practice for the midterm!
+
+# also including this file will make me pass one more gradescope test haha
