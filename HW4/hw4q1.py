74fe12a04faa74a85fdfc1d8b4d56db3b0c4c255
bpater1 <bpater1@jh.edu>
2023-10-28 09:39:14 -0400

    q1 mystery test not passing

--- HW4/hw4q1.py
+++ HW4/hw4q1.py
@@ -10,7 +10,7 @@ def min_edit_distance(T, P):
                 dp[i][j] = j
             elif j == 0:
                 dp[i][j] = i
-            elif T[i - 1] == P[j - 1] or P[j - 1] == '.':
+            elif P[j - 1] == '.' or T[i - 1] == P[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1]
             else:
                 dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
@@ -18,7 +18,7 @@ def min_edit_distance(T, P):
     return dp[m][n]
 
 if len(sys.argv) != 3:
-    print("Usage: python3 hw4q1.py input_file output_file")
+    print("Usage: python hw4q1.py input_file output_file")
     sys.exit(1)
 
 input_file = sys.argv[1]
@@ -29,10 +29,20 @@ try:
         T = file.readline().strip()
         P = file.readline().strip()
 
-    distance = min_edit_distance(T, P)
+    # Initialize the minimum distance to be a large value
+    min_distance = float('inf')
 
-    with open(output_file, 'w') as out_file:
-        out_file.write(str(distance) + '\n')
+    # Check if P is a prefix of T
+    if T.startswith(P):
+        min_distance = min(min_distance, len(T) - len(P))
+    
+    # Iterate through all substrings of T
+    for i in range(len(T)):
+        distance = min_edit_distance(T[i:], P)
+        min_distance = min(min_distance, distance)
+
+    with open(output_file, 'w') as output:
+        output.write(str(min_distance) + '\n')
 
 except FileNotFoundError:
     print(f"File '{input_file}' not found.")
