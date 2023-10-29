3cafe48f6f26b30a607696d33e37b31ced712211
bpater1 <bpater1@jh.edu>
2023-10-28 21:50:35 -0400

    q3 closer

--- HW4/hw4q3.py
+++ HW4/hw4q3.py
@@ -2,42 +2,49 @@ import sys
 
 def find_unitigs(input_file, output_file):
     # Read the "best unambiguous match to the right" information from the input file
-    matches = {}
+    bmr_matches = {}
+    bml_matches = {}
+
     with open(input_file, 'r') as file:
         for line in file:
             parts = line.strip().split()
             read_id, overlap, match_id = parts[0], int(parts[1]), parts[2]
-            if match_id not in matches:
-                matches[match_id] = {'BMR': [], 'BML': []}
-            matches[match_id]['BMR'].append((read_id, overlap))
-            if read_id not in matches:
-                matches[read_id] = {'BMR': [], 'BML': []}
-            matches[read_id]['BML'].append((match_id, overlap))
+
+            # Update BMR (best match to the right)
+            if read_id not in bmr_matches:
+                bmr_matches[read_id] = []
+            bmr_matches[read_id].append((match_id, overlap))
+
+            # Update BML (best match to the left)
+            if match_id not in bml_matches:
+                bml_matches[match_id] = []
+            bml_matches[match_id].append((read_id, overlap))
 
     unitigs = []
 
     def find_unitig(start_read):
         unitig = [start_read]
-        while start_read in matches:
-            matches[start_read]['BMR'].sort(key=lambda x: -x[1])
-            next_read, overlap = matches[start_read]['BMR'][0]
-            del matches[start_read]
-            if next_read in matches and (start_read, overlap) in matches[next_read]['BML']:
-                unitig.append(next_read)
+        while start_read in bmr_matches:
+            if not bmr_matches[start_read]:
+                break  # No BMR, exit the loop
+            bmr_matches[start_read].sort(key=lambda x: -x[1])  # Sort by descending overlap
+            next_read, overlap = bmr_matches[start_read][0]
+            unitig.append(next_read)
+            bmr_matches[start_read] = bmr_matches[start_read][1:]  # Remove the used match
+            if next_read in bml_matches and (start_read, overlap) in bml_matches[next_read]:
                 start_read = next_read
             else:
                 break
         return unitig
 
-
     # Find and build unitigs
-    for start_read in list(matches.keys()):  # Create a copy of keys
-        if start_read not in matches:
+    for start_read in list(bmr_matches.keys()):  # Create a copy of keys
+        if start_read not in bmr_matches:
             continue
         unitig = find_unitig(start_read)
         unitigs.append(unitig)
 
-    # Sort unitigs alphabetically by the leftmost read
+    # Sort unitigs in alphabetical order by the leftmost read
     unitigs.sort(key=lambda x: x[0])
 
     # Write the unitigs to the output file
@@ -46,7 +53,7 @@ def find_unitigs(input_file, output_file):
             out_file.write(unitig[0] + '\n')
             prev_read = unitig[0]
             for read in unitig[1:]:
-                overlap = next(x[1] for x in matches[prev_read] if x[0] == read)
+                overlap = next(x[1] for x in bml_matches[read] if x[0] == prev_read)
                 out_file.write(f"{overlap} {read}\n")
                 prev_read = read
 
