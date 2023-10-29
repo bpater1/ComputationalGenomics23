c8bda35d3caa7cec716619d90a3dae2bd508feab
bpater1 <bpater1@jh.edu>
2023-10-28 21:08:31 -0400

    q2 close

--- HW4/hw4q2.py
+++ HW4/hw4q2.py
@@ -13,16 +13,6 @@ def suffix_prefix_match(str1, str2, min_overlap):
         if str2.startswith(str1_suffix):
             return len(str1_suffix)
 
-def make_kmer_table(seqs, k):
-    table = {}
-    for name, seq in seqs.items():
-        for i in range(0, len(seq) - k + 1):
-            kmer = seq[i:i+k]
-            if kmer not in table:
-                table[kmer] = set()
-            table[kmer].add(name)
-    return table
-
 if len(sys.argv) != 4:
     print("Usage: python3 hw4q2.py input_fastq K output_file")
     sys.exit(1)
@@ -33,34 +23,24 @@ output_file = sys.argv[3]
 
 reads = {}
 current_read = None
-
 with open(input_fastq, 'r') as file:
     for line in file:
         if line.startswith('@'):
             current_read = line.strip()
-        else:
-            reads[current_read] = line.strip()
-
-kmer_table = make_kmer_table(reads, K)
+            sequence = next(file).strip()
+            reads[current_read] = sequence
 
 with open(output_file, 'w') as output:
     for read_name, read_seq in reads.items():
-        kmer = read_seq[:K]
-        potential_matches = set()
-        if kmer in kmer_table:
-            potential_matches = kmer_table[kmer]
-        
-        best_match = None
         max_overlap = 0
+        best_match = None
 
-        for match_name in potential_matches:
+        for match_name, match_seq in reads.items():
             if match_name != read_name:
-                overlap = suffix_prefix_match(read_seq, reads[match_name], K)
-                if overlap > max_overlap:
+                overlap = suffix_prefix_match(read_seq, match_seq, K)
+                if overlap >= max_overlap:
                     max_overlap = overlap
                     best_match = match_name
-        
+
         if best_match:
             output.write(f"{read_name[1:]} {max_overlap} {best_match[1:]}\n")
-
-print("Done.")
